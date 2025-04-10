568cd5ddbee76fdfe0bb19f544901778ff71e293
octavioromagnoli <octaromag@gmail.com>
2025-04-10 17:25:15 -0300

    Lanza Class

---
+++ ej2/armas/Combate/Lanza/Lanza.cpp
@@ -0,0 +1,69 @@
+#include <string>
+#include <utility>
+#include "Lanza.hpp"
+using namespace std;
+
+Lanza::Lanza(string name, int damage, Spirit spirit, int level, bool broken) : Combate(name, damage, spirit, level, broken) {}
+
+pair<string, int> Lanza::basicMove()
+{
+  return make_pair("Ataque de Lanza", 10);
+}
+
+pair<string, int> Lanza::spiritualMove()
+{
+  string retString = "Ataque débil de Lanza";
+  int retValue = 5;
+
+  switch (spirit)
+  {
+  case Spirit::None:
+    break;
+
+  case Spirit::Beast:
+    retString = "Zarpazo Perforador";
+    retValue = 30;
+    break;
+
+  case Spirit::Dragon:
+    retString = "Pinche Celestial";
+    retValue = 10;
+    break;
+
+  case Spirit::Shadow:
+    retString = "Emboscada Silenciosa";
+    retValue = 20;
+    break;
+  }
+
+  return make_pair(retString, retValue);
+}
+
+pair<string, int> Lanza::spiritualEffect()
+{
+  string retString = "Efecto débil de Lanza";
+  int retValue = 5;
+
+  switch (spirit)
+  {
+  case Spirit::None:
+    break;
+
+  case Spirit::Beast:
+    retString = "Marca Venenosa";
+    retValue = 15;
+    break;
+
+  case Spirit::Dragon:
+    retString = "Escamas Eternas";
+    retValue = 30;
+    break;
+
+  case Spirit::Shadow:
+    retString = "Silencio Mortal";
+    retValue = 20;
+    break;
+  }
+
+  return make_pair(retString, retValue);
+}
---
+++ ej2/armas/Combate/Lanza/Lanza.hpp
@@ -0,0 +1,18 @@
+#ifndef LANZA_H
+#define LANZA_H
+
+#include <string>
+#include <utility>
+#include "../Combate.hpp"
+using namespace std;
+
+class Lanza : public Combate
+{
+  Lanza(string name, int damage, Spirit spirit = Spirit::None, int level = 1, bool broken = false);
+
+  pair<string, int> basicMove() override;
+  pair<string, int> spiritualMove() override;
+  pair<string, int> spiritualEffect() override;
+};
+
+#endif // LANZA_H
