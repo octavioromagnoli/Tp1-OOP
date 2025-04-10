ae335d94f388a4b864b9f2f1f80288f23b99a047
octavioromagnoli <octaromag@gmail.com>
2025-04-10 15:35:22 -0300

    Amuleto Class

---
+++ ej2/armas/Magico/Amuleto/Amuleto.cpp
@@ -0,0 +1,69 @@
+#include <string>
+#include <utility>
+#include "Amuleto.hpp"
+using namespace std;
+
+Amuleto::Amuleto(string name, int damage, Element element, int level, bool broken) : Magico(name, damage, element, level, broken) {}
+
+pair<string, int> Amuleto::basicMove()
+{
+  return make_pair("Ataque de Amuleto", 10);
+}
+
+pair<string, int> Amuleto::elementalMove()
+{
+  string retString = "Ataque débil de Amuleto";
+  int retValue = 5;
+
+  switch (element)
+  {
+  case Element::None:
+    break;
+
+  case Element::Nature:
+    retString = "Llamado del Bosque";
+    retValue = 10;
+    break;
+
+  case Element::Darkness:
+    retString = "Marca Maldita";
+    retValue = 20;
+    break;
+
+  case Element::Luminic:
+    retString = "Aura Celestial";
+    retValue = 30;
+    break;
+  }
+
+  return make_pair(retString, retValue);
+}
+
+pair<string, int> Amuleto::magicalEffect()
+{
+  string retString = "Efecto débil de Amuleto";
+  int retValue = 5;
+
+  switch (element)
+  {
+  case Element::None:
+    break;
+
+  case Element::Nature:
+    retString = "Corazón del Bosque";
+    retValue = 30;
+    break;
+
+  case Element::Darkness:
+    retString = "Contrato Oscuro";
+    retValue = 15;
+    break;
+
+  case Element::Luminic:
+    retString = "Luz del Guardian";
+    retValue = 10;
+    break;
+  }
+
+  return make_pair(retString, retValue);
+}
---
+++ ej2/armas/Magico/Amuleto/Amuleto.hpp
@@ -0,0 +1,18 @@
+#ifndef AMULETO_HPP
+#define AMULETO_HPP
+
+#include <string>
+#include <utility>
+#include "../Magico.hpp"
+using namespace std;
+
+class Amuleto : public Magico
+{
+  Amuleto(string name, int damage, Element element = Element::None, int level = 1, bool broken = false);
+
+  pair<string, int> basicMove() override;
+  pair<string, int> elementalMove() override;
+  pair<string, int> magicalEffect() override;
+};
+
+#endif // AMULETO_HPP
