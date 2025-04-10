f6837dea6bf37b2c785af0e32fe9171824516ff0
octavioromagnoli <octaromag@gmail.com>
2025-04-10 15:48:29 -0300

    Virtual functions in Combate redeclared

--- ej2/armas/Combate/Combate.hpp
+++ ej2/armas/Combate/Combate.hpp
@@ -29,9 +29,20 @@ public:
   Spirit getSpirit() const;
   void setSpirit(Spirit);
 
-  virtual int basicMove() = 0;
-  virtual int spiritualMove() = 0;
-  virtual int spiritualEffect() = 0;
+  
+  /*
+    Retorna string con el ataque básico del arma y el daño que aplica.
+  */
+ virtual pair<string, int> basicMove() = 0;
+ /*
+   Retorna string con el ataque Elemental del arma y el daño que aplica.
+ */
+ virtual pair<string, int> spriritualMove() = 0;
+ /*
+   Retorna string con el efecto Mágico del arma y el daño que aplica.
+ */
+ virtual pair<string, int> spriritualEffect() = 0;
+
 
 protected:
   string name;
